fe4faa463386b4d3fcf7a6b14ca64df97d916f42
April Adams <illusionlace@gmail.com>
2020-09-01 14:45:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 || num == 0 || num == 1
+    return false 
+  elsif 
+    (2..num - 1).each do |i|
+      if num % i == 0 
+        return false 
+      end 
+    end
+  end 
+  true 
+end 
+    
+    
+  
